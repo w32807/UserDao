2ec1e506a52027b109c280c4dbc6d9a28fb3a5e5
w32807 <w32807@nate.com>
2022-03-23 21:29:44 +0900

    declare NUserDao and DUserDao

--- src/user/Application.java
+++ src/user/Application.java
@@ -2,11 +2,12 @@ package user;
 
 import java.sql.SQLException;
 
+import userDao.NUserDao;
 import userDao.UserDao;
 
 public class Application {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new UserDao();
+		UserDao dao = new NUserDao();
 		dao.delete();
 		dao.add(new User("aaa", "ภๅฟ๘มุ", "1234"));
 		
---
+++ src/userDao/DUserDao.java
@@ -0,0 +1,14 @@
+package userDao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao{
+
+	@Override
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+	}
+}
